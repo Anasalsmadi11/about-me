d485d087c3e19bc714ea5eac3f2bee14aa5283f9
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2021-04-16 21:08:16 +0300

    finishing and last check on aboute me page

---
+++ sdd.js
@@ -0,0 +1,24 @@
+function nameFunction(params){
+  console.log(params);
+  return params;
+}
+nameFunction('classs-02');
+
+
+/*const newFunctionWay= function(liftside,rightside){
+    console.log('asfabagg');
+    let sum =screenLeft+rightside;
+    return sum;
+}
+
+//let sum =newFunctionWay(newFunctionWay(3,4),5);
+console.log(sum);*/
+function syhi(){
+
+  //alert('afnljngfb b');
+  let hi='anas';
+}
+//syhi();
+
+
+
